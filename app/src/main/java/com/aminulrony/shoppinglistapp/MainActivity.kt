61c6e43318f270b12f9eb4f6966a466e076432d9
aminulrony2024 <aminulrony2024@gmail.com>
2024-10-07 17:21:32 +0600

    first commit

---
+++ app/src/main/java/com/aminulrony/shoppinglistapp/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.aminulrony.shoppinglistapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.aminulrony.shoppinglistapp.ui.theme.ShoppingListAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ShoppingListAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MyShoppingApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyShoppingApp(modifier: Modifier = Modifier) {
+    Text(text="My Shopping List",
+        modifier = modifier)
+}
